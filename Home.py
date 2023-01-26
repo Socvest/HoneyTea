f5abdfcfa0f68d147a7f9d58bf403b5552e89923
Socvest <82335390+Socvest@users.noreply.github.com>
2023-01-26 05:48:13 +0000

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.header("HoneyTea Business Development")
+st.write("This is a private app that you can use to build your analysis for the development of the company - later we can build a better one that is privately hosted and designed to do exactly what you want. But this is a quick one.")
