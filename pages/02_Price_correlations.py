22482cd0561430b47a84a364d23d03946972f88b
Socvest <82335390+Socvest@users.noreply.github.com>
2023-01-26 07:47:00 +0000

    Update 02_Price_correlations.py

--- pages/02_Price_correlations.py
+++ pages/02_Price_correlations.py
@@ -3,7 +3,14 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from sklearn.preprocessing import StandardScaler
-st.set_page_config(page_title="Plotting Demo", layout="wide", page_icon="📈")
+st.set_page_config(page_title="Plotting Demo", layout="wide")
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 def get_data_and_Clean():
     data = pd.read_csv("HoneyTea_Price_analysis_1.csv", encoding='unicode_escape', index_col=0)
