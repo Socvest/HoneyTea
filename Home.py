ff406887f0a026caa162806964c5ba9bc4d654f9
Socvest <82335390+Socvest@users.noreply.github.com>
2023-01-26 07:46:38 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,5 +2,12 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 st.header("HoneyTea Business Development")
 st.write("This is a private app that you can use to build your analysis for the development of the company - later we can build a better one that is privately hosted and designed to do exactly what you want. But this is a quick one.")
