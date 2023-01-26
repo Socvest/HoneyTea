7e8f865645556b2a7f93083f736be2e3c7ad7317
Socvest <82335390+Socvest@users.noreply.github.com>
2023-01-26 07:47:53 +0000

    Update 04_Predict.py

--- pages/04_Predict.py
+++ pages/04_Predict.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.set_page_config(page_title="Price Prediction", layout="wide", page_icon="📈")
+st.set_page_config(page_title="Price Prediction", layout="wide")
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 st.header("Coming soon...")
