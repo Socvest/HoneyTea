e79d60c4d2c0a8c202f6b0813631fb8ece3ca0ca
Socvest <82335390+Socvest@users.noreply.github.com>
2023-01-26 07:23:44 +0000

    Add files via upload

--- pages/01_Price_trends.py
+++ pages/01_Price_trends.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.set_page_config(page_title="Price Trends", layout="wide", page_icon="📈")
 
 def get_data_and_Clean():
-    data = pd.read_csv("HoneyTea_Price_analysis_1.csv", encoding='unicode_escape', index_col=0)
+    data = pd.read_csv("D:\HoneyTea\HoneyTea_Price_analysis_1.csv", encoding='unicode_escape', index_col=0)
     data = data[data.columns.drop(list(data.filter(regex='Unnamed')))]
     new_prices = data['prices of items on Amazon'].apply(lambda x: x.replace("Â",""))
     data['prices of items on Amazon'] = new_prices
@@ -53,12 +53,14 @@ def test(
     find_the_session_state="Col_1_find_the",
     of_session_state="Col_1_prices",
     chart_type_session_state="chart_type_column_1",
+    chart_column_1_plot = "chart_column_1_plot",
     load_session_state_col_2="column_2_submit",
     form_session_state_col_2="col_2_data_Filter", 
     for_every_session_state_col_2="Col_2_for_every", 
     find_the_session_state_col_2="Col_2_find_the",
     of_session_state_col_2="Col_2_prices",
     chart_type_session_state_2="chart_type_column_2",
+    chart_column_2_plot="chart_column_2_plot"
     ):
 
     main_Cols = st.columns(2)
@@ -79,7 +81,7 @@ def test(
             data_to_select.remove("prices of items on Amazon")
             st.selectbox(label="For every", options=data_to_select, key=for_every_session_state)
             # with cols[1]:
-            st.multiselect(label="find the", options=['mean', 'median', 'max', 'min', "std", "quartile", "count"], key=find_the_session_state)
+            st.multiselect(label="find the", options=['mean', 'median', 'max', 'min', "quartile"], key=find_the_session_state)
             # with cols[2]:
             st.selectbox(label="of", options=["prices of items on Amazon"], index=0, key=of_session_state)
             st.form_submit_button("Submit", on_click=col_1_submit)
@@ -90,35 +92,55 @@ def test(
             if st.session_state[load_session_state] == False or st.session_state[for_every_session_state] == [] or st.session_state[find_the_session_state] == []:
                 st.info("Fill in select boxes above to view table")
             
-            else:
-                if "quartile" in st.session_state[find_the_session_state]:
-                    stat_measures = st.session_state[find_the_session_state]
-                    stat_measures.remove("quartile")
-
-                    df1 = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg(stat_measures)
-
-                    def percentile(n):
-                        def percentile_(x):
-                            return np.percentile(x, n)
-                        percentile_.__name__ = 'percentile_%s' % n
-                        return percentile_
-                    
-                    df3 = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg([percentile(25), percentile(50), percentile(75)])
-                    df = pd.concat([df1, df3], axis=1).dropna()
+            elif st.session_state[load_session_state]:
+                if st.session_state[find_the_session_state] == []:
+                    st.error("Cannot have a blank selection")
                 else:
-                    df = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg(st.session_state[find_the_session_state]).dropna()
-                st.write(df, width=700)
+                
+                    if "quartile" in st.session_state[find_the_session_state]:
+                        stat_measures = st.session_state[find_the_session_state]
+                        stat_measures.remove("quartile")
+
+                        df1 = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg(stat_measures)
+
+                        def percentile(n):
+                            def percentile_(x):
+                                return np.percentile(x, n)
+                            percentile_.__name__ = 'percentile_%s' % n
+                            return percentile_
+                        
+                        df3 = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg([percentile(25), percentile(50), percentile(75)])
+                        df = pd.concat([df1, df3], axis=1).dropna()
+                    else:
+                        df = data.groupby(st.session_state[for_every_session_state])[st.session_state[of_session_state]].agg(st.session_state[find_the_session_state]).dropna()
+                    st.dataframe(df, width=700)
 
         with tabs1[1]:
-            if st.session_state[load_session_state] == False or st.session_state[for_every_session_state] == [] or st.session_state[find_the_session_state] == []:
-                st.info("Please fill in select boxes to view chart")
+            if st.session_state[load_session_state] == False: # st.session_state[for_every_session_state] == [] or st.session_state[find_the_session_state] == []:
                 
-            else:
-                st.radio("Chart type", options=["line", "bar"] , horizontal=True, key=chart_type_session_state)
-                if st.session_state[chart_type_session_state] == "line":
-                    st.line_chart(df) 
-                elif st.session_state[chart_type_session_state] == "bar":
-                    st.bar_chart(df)
+                st.info("Please fill in select boxes to view chart")
+
+            elif st.session_state[load_session_state]:
+                try:
+                    columns_to_plot = df.columns.tolist()
+                except:
+                    st.error("Cannot view chart with a blank selection")
+                    st.stop()
+                # if st.session_state[find_the_session_state] == []:
+                #     st.error("Cannot have a blank selection")
+                else:
+                    st.multiselect(label="Plot", options=columns_to_plot, default=columns_to_plot[0], key=chart_column_1_plot)
+                    st.radio("Chart type", options=["line", "bar"] , horizontal=True, key=chart_type_session_state)
+                    if st.session_state[chart_type_session_state] == "line":
+                        if st.session_state[chart_column_1_plot] == []:
+                            st.warning("Select data to plot")
+                        else:
+                            st.line_chart(df[st.session_state[chart_column_1_plot]]) 
+                    elif st.session_state[chart_type_session_state] == "bar":
+                        if st.session_state[chart_column_1_plot] == []:
+                            st.warning("Select data to plot")
+                        else:
+                            st.bar_chart(df[st.session_state[chart_column_1_plot]])
                 #, x=st.session_state['Col_1_for_every'][0], y=st.session_state['Col_1_prices'])
     
     with main_Cols[1]:
@@ -137,7 +159,7 @@ def test(
             data_to_select.remove("prices of items on Amazon")
             st.selectbox(label="For every", options=data_to_select, key=for_every_session_state_col_2)
             # with cols[1]:
-            st.multiselect(label="find the", options=['mean', 'median', 'max', 'min'], key=find_the_session_state_col_2)
+            st.multiselect(label="find the", options=['mean', 'median', 'max', 'min', "quartile"], key=find_the_session_state_col_2)
             # with cols[2]:
             st.selectbox(label="of", options=["prices of items on Amazon"], index=0, key=of_session_state_col_2)
             st.form_submit_button("Submit", on_click=col_2_submit)
@@ -148,27 +170,61 @@ def test(
             if st.session_state[load_session_state_col_2] == False or st.session_state[for_every_session_state_col_2] == [] or st.session_state[find_the_session_state_col_2] == []:
                 st.info("Fill in select boxes above to view table")
             
-            else:
+            elif st.session_state[load_session_state_col_2]:
+
+                if st.session_state[find_the_session_state_col_2] == []:
+                    st.error("Cannot have a blank selection")
+                else:
+                
+                    if "quartile" in st.session_state[find_the_session_state_col_2]:
+                        stat_measures = st.session_state[find_the_session_state_col_2]
+                        stat_measures.remove("quartile")
+
+                        df1 = data.groupby(st.session_state[for_every_session_state_col_2])[st.session_state[of_session_state_col_2]].agg(stat_measures)
+
+                        def percentile(n):
+                            def percentile_(x):
+                                return np.percentile(x, n)
+                            percentile_.__name__ = 'percentile_%s' % n
+                            return percentile_
+                        
+                        df3 = data.groupby(st.session_state[for_every_session_state_col_2])[st.session_state[of_session_state_col_2]].agg([percentile(25), percentile(50), percentile(75)])
+                        df = pd.concat([df1, df3], axis=1).dropna()
+                    else:
+                        df = data.groupby(st.session_state[for_every_session_state_col_2])[st.session_state[of_session_state_col_2]].agg(st.session_state[find_the_session_state_col_2]).dropna()
+                    st.dataframe(df, width=700)
+
                 df = data.groupby(st.session_state[for_every_session_state_col_2])[st.session_state[of_session_state_col_2]].agg(st.session_state[find_the_session_state_col_2]).dropna()
-                st.write(df, width=700)
+                st.dataframe(df, width=700)
 
         with tabs1[1]:
-            if st.session_state[load_session_state_col_2] == False or st.session_state[for_every_session_state_col_2] == [] or st.session_state[find_the_session_state_col_2] == []:
+            if st.session_state[load_session_state_col_2] == False: # or st.session_state[for_every_session_state_col_2] == [] or st.session_state[find_the_session_state_col_2] == []:
                 st.info("Please fill in select boxes to view chart")
                 
-            else:
-                st.radio("Chart type", options=["line", "bar"] , horizontal=True, key=chart_type_session_state_2)
-                if st.session_state[chart_type_session_state_2] == "line":
-                    st.line_chart(df) 
-                elif st.session_state[chart_type_session_state_2] == "bar":
-                    st.bar_chart(df)
+            elif st.session_state[load_session_state_col_2]:
+                
+                try:
+                    columns_to_plot = df.columns.tolist()
+                except:
+                    st.error("Cannot view chart with a blank selection")
+                    st.stop()
+                else:
+                    st.multiselect(label="Plot", options=columns_to_plot, default=columns_to_plot[0], key=chart_column_2_plot)
+                    st.radio("Chart type", options=["line", "bar"] , horizontal=True, key=chart_type_session_state_2)
+                    if st.session_state[chart_type_session_state_2] == "line":
+                        if st.session_state[chart_column_2_plot] == []:
+                            st.warning("Select data to plot")
+                        else:
+                            st.line_chart(df) 
+                    elif st.session_state[chart_type_session_state_2] == "bar":
+                        if st.session_state[chart_column_2_plot] == []:
+                            st.warning("Select data to plot")
+                        else:
+                            st.bar_chart(df)
                
  
 st.header("How price trends with other factors")
 test()
-
-# test(colNumber=1, load_session_state="column_2_submit", form_session_state="col_2_data_Filter", for_every_session_state="Col_2_for_every", 
-#     find_the_session_state="Col_2_find_the", of_session_state="Col_2_prices")
 
 st.write("")
 st.write("")
@@ -184,43 +240,15 @@ test(
     find_the_session_state="Col_3_find_the",
     of_session_state="Col_3_prices",
     chart_type_session_state = "chart_type_column_3",
+    chart_column_1_plot="chart_column_3_plot",
     load_session_state_col_2="column_4_submit",
     form_session_state_col_2="col_4_data_Filter", 
     for_every_session_state_col_2="Col_4_for_every", 
     find_the_session_state_col_2="Col_4_find_the",
     of_session_state_col_2="Col_4_prices",
-    chart_type_session_state_2 = "chart_type_column_4"
+    chart_type_session_state_2 = "chart_type_column_4",
+    chart_column_2_plot="chart_column_4_plot"
+
     )
 
 
-# st.header("Observe correlation between prices and other numerical factors") 
-# scatter_cols = st.columns([5,4])
-
-# with scatter_cols[0]:
-
-#     data_Types = data.dtypes.to_frame()
-#     data_Types_ = data_Types[(data_Types[0]== "float64") | (data_Types[0] == "int64")]
-#     if "scatter_plot_load" not in st.session_state:
-#             st.session_state["scatter_plot_load"] = False
-
-#     def col_scatter_submit():
-#         st.session_state["scatter_plot_load"] = True
-
-#     with st.form("scatter_chart"):
-#         st.selectbox("xAxis", options=data_Types_.index.tolist(), key="xAxis_")
-#         st.selectbox("yAxis", options=data_Types_.index.tolist(), key="yAxis_")
-#         st.form_submit_button("Submit", on_click=col_scatter_submit)
-
-#     if st.session_state["scatter_plot_load"] == False or st.session_state['yAxis_'] == "" or st.session_state["xAxis_"] == "":
-#         st.info("Please fill in the above to view the results")
-#     else:
-#         fig = px.scatter(data, x=st.session_state['xAxis_'], y=st.session_state['yAxis_'])
-#         st.plotly_chart(fig, use_container_width=True)
-
-    
-
-# with scatter_cols[1]:
-#     with st.form("Heatmap chart"):
-#         st.selectbox("xAxis", options=["Hi"])
-#         st.selectbox("yAxis", options=["hiya"])
-#         st.form_submit_button("Submit")
